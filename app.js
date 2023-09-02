404d62e306fdf1ae55e79b3989a18f2bea8b183a
Romcro <romualdcrochat@gmail.com>
2023-09-02 23:30:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,42 @@ const BMIData = [
 ];
 
 // IMC = poids en kg / taille² en m
+
+
+//ici on manipule le DOM en appelant le formulaire
+const form = document.querySelector("form")
+//ici on ajoute un evenement
+form.addEventListener("submit", handleForm)
+//ici on créer une fonction qui va gérer l'evenement
+function handleForm(event) {
+
+  event.preventDefault();
+//ici on appelle la fonction
+  calculateBMI()
+
+}
+//ici on appelle les inputs
+const inputs = document.querySelectorAll("input")
+console.log(inputs)
+//ici on créer une fonction qui va calculer l'IMC
+function calculateBMI() {
+//ici on appelle les inputs dans la fonction
+  const height = inputs[0].value
+  const weight = inputs[1].value
+
+//ici on appelle la fonction dans la console
+if (!height || !weight || height <= 0 || weight <= 0) {
+  handleError()
+  return;
+}
+
+}
+//ici on appelle le resultat
+const displayBMI = document.querySelector(".bmi-value");
+//ici on appelle le resultat
+const result = document.querySelector(".bmi-result"); 
+//ici on créer une fonction qui va gérer les erreurs
+function handleError() {
+  displayBMI.textContent = "Oups, il y a une erreur !"
+  result.textContent = "Remplissez correctement les champs!"
+}
